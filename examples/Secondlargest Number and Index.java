d129cc945ec391ad00619239505b2e158c965749
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-01 16:28:31 +0400

    Update Secondlargest Number and Index.java

--- examples/Secondlargest Number and Index.java
+++ examples/Secondlargest Number and Index.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class Calculator {
+public class SecondLagerst {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         Random rnd =  new Random();
