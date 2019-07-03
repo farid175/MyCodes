8e09a4e6d6bfc0b6d708a7fa9b4862359d1c7b69
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-03 15:24:05 +0400

    Create Move All 0 to EOArray.java

---
+++ examples/Move All 0 to EOArray.java
@@ -0,0 +1,29 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Massivin olcusunu daxil et:");
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 20);
+        }
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] == 0 && arr[j] != 0){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.print(arr[i] + " ");
+        }
+        
+    }
+}
