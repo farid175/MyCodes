b14a71838d6401319f903e2f28e776f6cd5046cc
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-03 17:08:08 +0400

    Create Rearrenge even and odd in Array.java

---
+++ examples/Rearrenge even and odd in Array.java
@@ -0,0 +1,35 @@
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Massivin olcusunu daxil et:");
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Massivi doldur:");
+        for (int i = 0; i < arr.length; i++) {
+           arr[i] = (int) (Math.random() * 20);
+        }
+
+        int l = 0,r = arr.length - 1,temp;
+
+        while(l < r){
+            if(arr[l]%2 == 0){
+                l++;
+            }else{
+                temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                r--;
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
