b6b8998146643570994b14f30c157d7e339a3729
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-03 13:46:01 +0400

    Create Sum of Two Elements in Array.java

---
+++ examples/Sum of Two Elements in Array.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Massivin olcusunu daxil et:");
+        int N = scan.nextInt();
+        System.out.println("Cemi olacaq reqemi daxil et:");
+        int K = scan.nextInt();
+
+        int[] arr = new int[N];
+
+        System.out.println("Massivi doldur:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println("Netice:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(arr[i] + arr[j] == K) {
+                    System.out.print("{" + arr[i] + " " + arr[j] + "}");
+                }
+            }
+        }
+
+    }
+}
