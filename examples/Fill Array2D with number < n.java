18456f1a7c0ed64cf4fc5f1e0198c2ce1f9f5a63
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-09 17:10:02 +0400

    Update Fill Array2D with number < n.java

--- examples/Fill Array2D with number < n.java
+++ examples/Fill Array2D with number < n.java
@@ -16,7 +16,6 @@ public class twodimensionalArr {
         }
 
         for (int i = 0; i < arr.length; i++) {
-            int k = 0;
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] +" ");
             }
