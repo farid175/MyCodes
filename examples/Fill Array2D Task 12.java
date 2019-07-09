723026e03abdbb236a3f4c260018b55ea0fa05ba
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-09 17:39:36 +0400

    Create Fill Array2D Task 12.java

---
+++ examples/Fill Array2D Task 12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class twodimensionalArr {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Size of Array:");
+        int n = scan.nextInt();
+        int[][] arr = new int[n][n];
+        int k = 1;
+            for (int i = arr.length-1; i >= 0; i--) {
+                    for(int j = 0;j < arr[i].length;j++){
+                        arr[i][j] = k;
+                        k++;
+                    }
+                }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
