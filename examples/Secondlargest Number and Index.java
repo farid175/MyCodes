ddfc98e75fe9464f5327bc2b1c8050c3253db844
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-19 14:32:13 +0400

    Update Secondlargest Number and Index.java

--- examples/Secondlargest Number and Index.java
+++ examples/Secondlargest Number and Index.java
@@ -1,40 +1,41 @@
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
-
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
+public class MainClass {
 
-public class SecondLagerst {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Random rnd =  new Random();
         int count = 0;
         System.out.print("Enter Size of Array:");
         int n = scan.nextInt();
         int[] arr = new int[n];
-        int pos = 0;
+
         for(int i = 0; i < arr.length;i++){
             arr[i] = rnd.nextInt(10);
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+
         int largest = arr[0];
-        int secondlargest = arr[0];
-
-        for(int i = 0;i < arr.length;i++){
-            if(arr[i] > largest){
-                secondlargest = largest;
+        for(int i = 1; i < arr.length; i++){
+            if(largest < arr[i]){
                 largest = arr[i];
+            }
+        }
 
-            }else if(arr[i] > secondlargest && arr[i] != largest){
+        int secondlargest = 0;
+        int pos = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != largest && (pos == -1 || arr[i] > secondlargest)){
                 secondlargest = arr[i];
                 pos = i;
             }
         }
 
-        System.out.println("Second Largest number:" +secondlargest + " Position:" +pos);
-
-
-
+        System.out.println("Second Largest number:" +secondlargest + " Position:" + pos);
     }
+
 }
