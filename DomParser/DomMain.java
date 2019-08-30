5168da87404be64f8788131406bbcdbc32242047
farid175 <50070768+farid175@users.noreply.github.com>
2019-08-30 16:34:42 +0400

    Update DomMain.java

--- DomParser/DomMain.java
+++ DomParser/DomMain.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class DomMain {
     public static void main(String[] args) {
-        try {
+       ry {
 
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
@@ -37,32 +37,41 @@ public class DomMain {
 
                     Seed seed = new Seed();
 
-                    String data = element.getElementsByTagName("ID").item(0).toString();
-                    seed.setID(Long.parseLong(data));
+                    String data = element.getElementsByTagName("ID").item(0).getTextContent();
+                    if (data != null) {
+                        seed.setID(Long.parseLong(data));
+                    } else {
+                        System.out.println("id is null");
+                    }
 
-                    data = element.getElementsByTagName("NAME").item(0).toString();
+                    data = element.getElementsByTagName("NAME").item(0).getTextContent();
                     seed.setNAME(data);
 
-                    data = element.getElementsByTagName("CODE").item(0).toString();
+                    data = element.getElementsByTagName("CODE").item(0).getTextContent();
                     seed.setCODE(Long.parseLong(data));
 
-                    data = element.getElementsByTagName("HIBRID_NAME").item(0).toString();
+                    data = element.getElementsByTagName("HIBRID_NAME").item(0).getTextContent();
                     seed.setHIBRID_NAME(data);
 
-                    data = element.getElementsByTagName("PATENT_NUMBER").item(0).toString();
+                    data = element.getElementsByTagName("PATENT_NUMBER").item(0).getTextContent();
                     seed.setPATENT_NUMBER(data);
 
-                    data = element.getElementsByTagName("START_DATE").item(0).toString();
-                    seed.setSTART_DATE(LocalDate.parse(data));
-
-                    data = element.getElementsByTagName("PATENT_OWNER").item(0).toString();
+                    NodeList nodeList1 = element.getElementsByTagName("START_DATE");
+                    if (nodeList1.getLength() > 0){
+                        data = element.getElementsByTagName("START_DATE").item(0).getTextContent();
+//                    seed.setSTART_DATE(LocalDate.parse(data));
+                        seed.setSTART_DATE(data);
+                    }
+                    data = element.getElementsByTagName("PATENT_OWNER").item(0).getTextContent();
                     seed.setPATENT_OWNER(data);
 
-                    data = element.getElementsByTagName("END_DATE").item(0).toString();
-                    seed.setEND_DATE(LocalDate.parse(data));
+                    data = element.getElementsByTagName("END_DATE").item(0).getTextContent();
+//                    seed.setEND_DATE(LocalDate.parse(data));
+                    seed.setEND_DATE(data);
 
-                    data = element.getElementsByTagName("START_REGISTER_DATE").item(0).toString();
-                    seed.setSTART_REGISTER_DATE(Long.parseLong(data));
+                    data = element.getElementsByTagName("START_REGISTER_DATE").item(0).getTextContent();
+//                    seed.setSTART_REGISTER_DATE(Long.parseLong(data));
+                    seed.setSTART_REGISTER_DATE(data);
 
                     seedList.add(seed);
 
