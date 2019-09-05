52dcb3bd3c3762c4b28755fbb4df7a271e2b736f
farid175 <50070768+farid175@users.noreply.github.com>
2019-09-05 14:32:40 +0400

    Create Seeds.java

---
+++ JAXBParser/Seeds.java
@@ -0,0 +1,31 @@
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "SEEDS")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Seeds {
+    @XmlElement(name = "body")
+    private Body body;
+
+    @XmlElement(name = "header")
+    private Header header;
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+}
